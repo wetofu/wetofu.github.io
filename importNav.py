10d80bca0e992c8f86648a84059c3fa8ab793111
carloz <msyahrul15aulia@gmail.com>
2020-12-29 09:42:22 +0700

    updating

--- importNav.py
+++ importNav.py
@@ -1,11 +1,13 @@
-listFile = ["index.html", "print.html", "00-intro/index.html", "00-intro/inf.html", "00-intro/kimia.html", 
-			"02-cybersec/deepweeb.html", "02-cybersec/footprinting.html", "02-cybersec/index.html", "02-cybersec/infgather.html",
-			"02-cybersec/tcpip.html", "03-ctf/index.html", "03-ctf/binex.html", "03-ctf/binex-pwnshell.html", "03-ctf/assembly.html",  
-			"04-tips/index.html", "05-jarkom/networking.html", "05-jarkom/basicdatasec.html", "05-jarkom/cmlNwireshark.html", 
-			"05-jarkom/dsnpi.html", "05-jarkom/email.html", "05-jarkom/hacking.html", "05-jarkom/index.html", "05-jarkom/ipv6.html", 
-			"05-jarkom/keamananJar.html", "05-jarkom/nlipv4.html", "05-jarkom/nmb.html", "05-jarkom/pemjar.html", 
-			"05-jarkom/pendahuluan.html", "05-jarkom/pkjdll.html", "05-jarkom/pwwi.html", "05-jarkom/rnat.html", 
-			"05-jarkom/simJar.html", "05-jarkom/sm.html", "05-jarkom/tl.html", "05-jarkom/voip.html", "05-jarkom/webcms.html", 
+listFile = ["index.html", "print.html", 
+			"00-intro/index.html", "00-intro/inf.html", "00-intro/kimia.html", "00-intro/pbo.html", "00-intro/basdat.html",
+			"02-cybersec/deepweeb.html", "02-cybersec/footprinting.html", "02-cybersec/index.html", "02-cybersec/infgather.html", "02-cybersec/tcpip.html", 
+			"03-ctf/index.html", "03-ctf/binex.html", "03-ctf/binex-pwnshell.html", "03-ctf/assembly.html",  
+			"04-tips/index.html", 
+			"05-jarkom/networking.html", "05-jarkom/basicdatasec.html", "05-jarkom/cmlNwireshark.html", 
+				"05-jarkom/dsnpi.html", "05-jarkom/email.html", "05-jarkom/hacking.html", "05-jarkom/index.html", "05-jarkom/ipv6.html", 
+				"05-jarkom/keamananJar.html", "05-jarkom/nlipv4.html", "05-jarkom/nmb.html", "05-jarkom/pemjar.html", 
+				"05-jarkom/pendahuluan.html", "05-jarkom/pkjdll.html", "05-jarkom/pwwi.html", "05-jarkom/rnat.html", 
+				"05-jarkom/simJar.html", "05-jarkom/sm.html", "05-jarkom/tl.html", "05-jarkom/voip.html", "05-jarkom/webcms.html", 
 			"06-tools/nmap.html", "06-tools/index.html"]	# ini diisi file oprec
 baseFile = "base/baseNav.html"
 firstTextLooking = "<!-- ---------------------------------------- NAV ---------------------------------------- -->"
