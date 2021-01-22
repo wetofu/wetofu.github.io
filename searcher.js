9e988a9f88fe50ea3ead54469b5068a80d247692
wetofu <msyahrul15aulia@gmail.com>
2021-01-22 07:23:44 +0700

    updating

--- searcher.js
+++ searcher.js
@@ -1,34 +1,18 @@
-// var search_input = document.querySelector("#search_input");
+// source from https://www.w3schools.com/
 
-// search_input.addEventListener("keyup", function(e){
-//   var span_items = document.querySelectorAll("li a");
-//   var table_body = document.querySelector(".table_body ul");
-//   var search_item = e.target.value.toLowerCase();
- 
-//  span_items.forEach(function(item){
-//    if(item.textContent.toLowerCase().indexOf(search_item) != -1){
-//       item.closest("li").style.display = "block";
-//    }
-//    else{
-//      item.closest("li").style.display = "none";
-//      }
-//  })
-  
-// });
-// function myFunction() {
-//     var input, filter, ul, li, a, i, txtValue;
-//     input = document.getElementById("search_input");
-//     filter = input.value.toUpperCase();
-//     ul = document.getElementById("myUL");
-//     li = ul.getElementsByTagName("li");
-//     // li = ul.getElementById("gantii");    
-//     for (i = 0; i < li.length; i++) {
-//         a = li[i].getElementsByTagName("a")[0];
-//         txtValue = a.textContent || a.innerText;
-//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//             li[i].style.display = "";
-//         } else {
-//             li[i].style.display = "none";
-//         }
-//     }
-// }
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByClassName("expanded");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("span")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
